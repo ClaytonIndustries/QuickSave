3f78f5a8b762bcc57bcdf96a91e224ebeed2ee9c
Michael Clayton <mike-16@hotmail.co.uk>
2017-12-14 22:49:59 +0000

    Remove code duplication from raw

--- Source/Assets/QuickSave/QuickSaveRaw.cs
+++ Source/Assets/QuickSave/QuickSaveRaw.cs
@@ -7,16 +7,7 @@ namespace CI.QuickSave
     {
         public static void Save<T>(string root, T value)
         {
-            try
-            {
-                string jsonToSave = JsonSerialiser.Serialise(value);
-
-                FileAccess.Save(root, jsonToSave);
-            }
-            catch (Exception e)
-            {
-                throw new InvalidOperationException("Unable to save data", e);
-            }
+            Save(root, value, new QuickSaveSettings());
         }
 
         public static void Save<T>(string root, T value, QuickSaveSettings settings)
@@ -37,16 +28,7 @@ namespace CI.QuickSave
 
         public static T Load<T>(string root)
         {
-            try
-            {
-                string fileJson = FileAccess.Load(root);
-
-                return JsonSerialiser.Deserialise<T>(fileJson);
-            }
-            catch (Exception e)
-            {
-                throw new InvalidOperationException("Unable to load data", e);
-            }
+            return Load<T>(root, new QuickSaveSettings());
         }
 
         public static T Load<T>(string root, QuickSaveSettings settings)
