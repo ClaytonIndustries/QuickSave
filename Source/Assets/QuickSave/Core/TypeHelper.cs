72b1f7e001f5e7a3e430e618380256bcc76f7dd5
Michael Clayton <mike-16@hotmail.co.uk>
2017-12-16 16:22:58 +0000

    Check for unity types

---
+++ Source/Assets/QuickSave/Core/TypeHelper.cs
@@ -0,0 +1,28 @@
+﻿using System.Linq;
+using System.Collections.Generic;
+
+namespace CI.QuickSave.Core
+{
+    public static class TypeHelper
+    {
+        private static IEnumerable<string> _unityTypes = new List<string>()
+        {
+            "UnityEngine.Vector2",
+            "UnityEngine.Vector3",
+            "UnityEngine.Vector4",
+            "UnityEngine.Quaternion",
+            "UnityEngine.Color",
+            "UnityEngine.Color32",
+            "UnityEngine.Rect",
+            "UnityEngine.Bounds",
+            "UnityEngine.Matrix4x4"
+        };
+
+        public static bool IsUnityType<T>(T value)
+        {
+            string typename = value.GetType().FullName;
+
+            return _unityTypes.Any(x => x == typename);
+        }
+    }
+}
