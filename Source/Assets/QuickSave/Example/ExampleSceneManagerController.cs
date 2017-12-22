f28d48656f8478d175c5e7380e86cd36fc83ace7
Michael Clayton <mike-16@hotmail.co.uk>
2017-12-22 10:31:28 +0000

    Update example scene

--- Source/Assets/QuickSave/Example/ExampleSceneManagerController.cs
+++ Source/Assets/QuickSave/Example/ExampleSceneManagerController.cs
@@ -1,51 +1,38 @@
-﻿using System.Collections.Generic;
-using CI.QuickSave;
+﻿using CI.QuickSave;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ExampleSceneManagerController : MonoBehaviour
 {
+    public InputField Input1;
+    public InputField Input2;
+    public InputField Input3;
+    public InputField Input4;
+    public InputField Input5;
+    public InputField Input6;
+    public InputField Input7;
+    public InputField Input8;
+    public InputField Content;
+
     public void Save()
     {
-        Class1 one = new Class1()
-        {
-            ItemD = 12,
-            ItemE = "K",
-            ItemC = 34.6789f,
-            ItemB = 454545,
-            ItemA = "Hello World!!!!",
-            ItemF = 34.56,
-            ItemG = new List<int>() { 1, 2, 3, 4, 5, 6 },
-            ItemH = new List<IEnumerable<int>>()
-            {
-                new List<int>() { 1, 2, 3, 4, 5 },
-                new List<int>() { 6, 7, 8, 9, 0 }
-            }
-        };
-
-        Class2 two = new Class2()
-        {
-            KeyA = 12
-        };
-
-        Vector2 vector2 = new Vector2(1.5678f, 2f);
-
-        QuickSaveWriter.Create("Root2")
-                       .Write("Key1", one)
-                       .Write("Key2", two)
-                       .Write("Key3", vector2)
+        QuickSaveWriter.Create("Inputs")
+                       .Write("Input1", Input1.text)
+                       .Write("Input2", Input2.text)
+                       .Write("Input3", Input3.text)
+                       .Write("Input4", Input4.text)
                        .Commit();
+
+        Content.text = QuickSaveRaw.LoadString("Inputs.json");
     }
 
     public void Load()
     {
-        Class1 class1;
-        Class2 class2;
-        Vector2 vector2;
-
-        QuickSaveReader.Create("Root2")
-                       .Read<Class1>("Key1", (r) => { class1 = r; })
-                       .Read<Class2>("Key2", (r) => { class2 = r; })
-                       .Read<Vector2>("Key3", (r) => { vector2 = r; });
+        QuickSaveReader.Create("Inputs")
+                       .Read<string>("Input1", (r) => { Input5.text = r; })
+                       .Read<string>("Input2", (r) => { Input6.text = r; })
+                       .Read<string>("Input3", (r) => { Input7.text = r; })
+                       .Read<string>("Input4", (r) => { Input8.text = r; });
     }
 
     public void QuickSaveRootExample()
@@ -58,7 +45,9 @@ public class ExampleSceneManagerController : MonoBehaviour
 
         QuickSaveRoot.Save("RootName", texture2D);
 
+#pragma warning disable 0219
         Texture2D loaded = QuickSaveRoot.Load<Texture2D>("RootName");
+#pragma warning restore 0219
     }
 
     public void QuickSaveRawExample()
@@ -68,8 +57,10 @@ public class ExampleSceneManagerController : MonoBehaviour
         QuickSaveRaw.SaveString("TextFile.txt", "Some text to save");
         QuickSaveRaw.SaveBytes("BytesFile.txt", new byte[] { 1, 2, 3, 4 });
 
+#pragma warning disable 0219
         string text = QuickSaveRaw.LoadString("TextFile.txt");
         byte[] bytes = QuickSaveRaw.LoadBytes("BytesFile.txt");
+#pragma warning restore 0219
     }
 
     public void QuickSaveReaderExample()
@@ -78,10 +69,12 @@ public class ExampleSceneManagerController : MonoBehaviour
 
         // An exception will be thrown if the root doesn't exist
 
+#pragma warning disable 0219
         string one;
         double two;
         Vector2 three;
         Color four;
+#pragma warning restore 0219
 
         QuickSaveReader.Create("RootName")
                        .Read<string>("Key1", (r) => { one = r; })
@@ -123,21 +116,4 @@ public class ExampleSceneManagerController : MonoBehaviour
         writer.Write("Key4", four);
         writer.Commit();
     }
-}
-
-public class Class1
-{
-    public string ItemA { get; set; }
-    public int ItemB { get; set; }
-    public double ItemC { get; set; }
-    public int ItemD { get; set; }
-    public string ItemE { get; set; }
-    public double ItemF { get; set; }
-    public IEnumerable<int> ItemG { get; set; }
-    public IEnumerable<IEnumerable<int>> ItemH { get; set; }
-}
-
-public class Class2
-{
-    public int KeyA { get; set; }
 }
