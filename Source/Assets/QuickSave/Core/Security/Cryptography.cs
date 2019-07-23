4a98f0b8799136d7579262fb546d7290d57349d3
Michael Clayton <mike-16@hotmail.co.uk>
2019-07-23 21:35:22 +0100

    Remove uwp cryptography

--- Source/Assets/QuickSave/Core/Security/Cryptography.cs
+++ Source/Assets/QuickSave/Core/Security/Cryptography.cs
@@ -6,24 +6,106 @@
 //
 ////////////////////////////////////////////////////////////////////////////////
 
+using System;
+using System.IO;
+using System.Security.Cryptography;
+using System.Text;
+
 namespace CI.QuickSave.Core.Security
 {
     public static class Cryptography
     {
-#if !NETFX_CORE
-        private static ICryptography _cryptography = new CryptographyMono();
-#else
-        private static ICryptography _cryptography = new CryptographyUWP();
-#endif
-
         public static string Encrypt(string value, SecurityMode securityMode, string password)
         {
-            return _cryptography.Encrypt(value, securityMode, password);
+            switch (securityMode)
+            {
+                case SecurityMode.Aes:
+                    return AesEncrypt(password, value);
+                case SecurityMode.Base64:
+                    return Base64Encode(value);
+                default:
+                    return value;
+            }
         }
 
         public static string Decrypt(string value, SecurityMode securityMode, string password)
         {
-            return _cryptography.Decrypt(value, securityMode, password);
+            switch (securityMode)
+            {
+                case SecurityMode.Aes:
+                    return AesDecrypt(password, value);
+                case SecurityMode.Base64:
+                    return Base64Decode(value);
+                default:
+                    return value;
+            }
+        }
+
+        private static string AesEncrypt(string encryptionKey, string value)
+        {
+            if (string.IsNullOrEmpty(value))
+            {
+                return value;
+            }
+
+            using (Aes encryptor = Aes.Create())
+            {
+                Rfc2898DeriveBytes pdb = new Rfc2898DeriveBytes(encryptionKey, new byte[] { 0x49, 0x76, 0x61, 0x6e, 0x20, 0x4d, 0x65, 0x64, 0x76, 0x65, 0x64, 0x65, 0x76 });
+
+                encryptor.Key = pdb.GetBytes(32);
+                encryptor.IV = pdb.GetBytes(16);
+
+                byte[] data = Encoding.UTF8.GetBytes(value);
+
+                using (MemoryStream ms = new MemoryStream())
+                {
+                    using (CryptoStream cs = new CryptoStream(ms, encryptor.CreateEncryptor(), CryptoStreamMode.Write))
+                    {
+                        cs.Write(data, 0, data.Length);
+                    }
+
+                    return Convert.ToBase64String(ms.ToArray());
+                }
+            }
+        }
+
+        private static string AesDecrypt(string encryptionKey, string value)
+        {
+            if (string.IsNullOrEmpty(value))
+            {
+                return value;
+            }
+
+            using (Aes decryptor = Aes.Create())
+            {
+                Rfc2898DeriveBytes pdb = new Rfc2898DeriveBytes(encryptionKey, new byte[] { 0x49, 0x76, 0x61, 0x6e, 0x20, 0x4d, 0x65, 0x64, 0x76, 0x65, 0x64, 0x65, 0x76 });
+
+                decryptor.Key = pdb.GetBytes(32);
+                decryptor.IV = pdb.GetBytes(16);
+
+                byte[] data = Convert.FromBase64String(value);
+
+                using (MemoryStream ms = new MemoryStream(data))
+                {
+                    using (CryptoStream cs = new CryptoStream(ms, decryptor.CreateDecryptor(), CryptoStreamMode.Read))
+                    {
+                        using (StreamReader reader = new StreamReader(cs))
+                        {
+                            return reader.ReadToEnd();
+                        }
+                    }
+                }
+            }
+        }
+
+        private static string Base64Encode(string value)
+        {
+            return Convert.ToBase64String(Encoding.UTF8.GetBytes(value));
+        }
+
+        private static string Base64Decode(string value)
+        {
+            return Encoding.UTF8.GetString(Convert.FromBase64String(value));
         }
     }
 }
